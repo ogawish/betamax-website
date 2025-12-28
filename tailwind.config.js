41c91fe5972ec1e4bb6e0d555e725aea1a629435
Omar Gawish <omar.gawish@indigen.com>
2025-12-28 13:01:49 +0200

    Edit main header for mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
+    `./src/layout/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
     extend: {},
